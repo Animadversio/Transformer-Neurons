5712dea7e089be2aab3bcbd1590836a197792a5e
BinxuWang <binxu.wang@wustl.edu>
2022-12-06 18:18:49 -0600

    Grad tracing experiments

---
+++ core/GPT-XL_grad_tracing.py
@@ -0,0 +1,320 @@
+"""
+Understand BERT and GPT via gradient tricks
+"""
+import os
+import warnings
+from os.path import join
+import torch
+import torch.nn as nn  # import ModuleList
+from tqdm import tqdm
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from copy import deepcopy
+from transformers.models.bert.modeling_bert import BertModel
+from transformers.models.gpt2.modeling_gpt2 import GPT2Model
+from transformers import BertModel, BertForMaskedLM, BertConfig, BertTokenizer
+from core.layer_hook_utils import featureFetcher_module
+from core.interp_utils import top_tokens_based_on_activation, topk_decode
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2-xl")
+model = GPT2LMHeadModel.from_pretrained("gpt2-xl")
+# Accessing the model configuration
+configuration = model.config
+model.requires_grad_(False)
+model.eval()
+#%%
+def get_causal_grad_hook(key, gradvar_store):
+    def causal_grad(module, input, output):
+        """Substitute the output of certain layer as a fixed representation
+        Note: Assume the first output variable is used in downstream computing.
+        """
+        gradvar = torch.zeros_like(output[0].data, requires_grad=True)
+        # output[0] + gradvar
+        output[0].add_(gradvar)
+        # output[0] = torch.autograd.Variable(output[0].data, requires_grad=True)
+        gradvar_store[key] = gradvar
+    return causal_grad
+
+
+def get_record_causal_grad_hook(key, gradvar_store, act_store):
+    def causal_grad(module, input, output):
+        """Substitute the output of certain layer as a fixed representation
+        Note: Assume the first output variable is used in downstream computing.
+        """
+        gradvar = torch.zeros_like(output[0].data, requires_grad=True)
+        # output[0] + gradvar
+        output[0].add_(gradvar)
+        # output[0] = torch.autograd.Variable(output[0].data, requires_grad=True)
+        gradvar_store[key] = gradvar
+        act_store[key] = output[0].detach().data.cpu()
+    return causal_grad
+
+
+def forward_gradhook_vars(model, token_ids, module_str="block",
+                          module_fun=None):
+    if module_str == "block":
+        module_fun = lambda model, layeri: model.transformer.h[layeri]
+    elif module_str == "attn":
+        module_fun = lambda model, layeri: model.transformer.h[layeri].attn
+    elif module_str == "mlp":
+        module_fun = lambda model, layeri: model.transformer.h[layeri].mlp
+    elif module_str == "ln_2":
+        module_fun = lambda model, layeri: model.transformer.h[layeri].ln_2
+    else:
+        if module_fun is None:
+            raise NotImplementedError
+        else:
+            module_fun = module_fun
+            warnings.warn("not recognized module_str, use the custom module function")
+    gradvar_store = {}  # store the gradient variable, layer num as key
+    hook_hs = []
+    for layeri in range(len(model.transformer.h)):
+        target_module = module_fun(model, layeri)  # model.transformer.h[layeri]
+        hook_fun = get_causal_grad_hook(layeri, gradvar_store)  # get_clamp_hook(fixed_hidden_states, fix_mask)
+        hook_h = target_module.register_forward_hook(hook_fun)
+        hook_hs.append(hook_h)
+
+    # with torch.no_grad():
+    try:
+        outputs = model(token_ids, output_hidden_states=True)
+    except:
+        # Need to remove hooks or it will keep raising error.
+        for hook_h in hook_hs:
+            hook_h.remove()
+        raise
+    else:
+        for hook_h in hook_hs:
+            hook_h.remove()
+        return outputs, gradvar_store
+
+
+from easydict import EasyDict as edict
+def forward_record_gradhook_vars_all(model, token_ids, ):
+    module_funs = {
+        "block": lambda model, layeri: model.transformer.h[layeri],
+        "attn":  lambda model, layeri: model.transformer.h[layeri].attn,
+        "mlp":   lambda model, layeri: model.transformer.h[layeri].mlp,
+        "ln_2":  lambda model, layeri: model.transformer.h[layeri].ln_2,
+    }
+    gradvar_stores = {}  # store the gradient variable, layer num as key
+    act_stores = {}
+    for k in module_funs:
+        gradvar_stores[k] = {}
+        act_stores[k] = {}
+    hook_hs = []
+    for layeri in range(len(model.transformer.h)):
+        for module_str, module_fun in module_funs.items():
+            target_module = module_fun(model, layeri)  #model.transformer.h[layeri]  # model.transformer.h[layeri]
+            hook_fun = get_record_causal_grad_hook(layeri, gradvar_stores[module_str], act_stores[module_str])  # get_clamp_hook(fixed_hidden_states, fix_mask)
+            hook_h = target_module.register_forward_hook(hook_fun)
+            hook_hs.append(hook_h)
+
+    # with torch.no_grad():
+    try:
+        outputs = model(token_ids, output_hidden_states=True)
+    except:
+        # Need to remove hooks or it will keep raising error.
+        for hook_h in hook_hs:
+            hook_h.remove()
+        raise
+    else:
+        for hook_h in hook_hs:
+            hook_h.remove()
+        return outputs, gradvar_stores, act_stores
+
+savedir = r"F:\insilico_exps\GPT-XL_grad_trace"
+#%%
+
+text = "The official residence of the Pope, Vatican is located in the city of"
+token_ids = tokenizer.encode(text, return_tensors='pt')
+tokens = tokenizer.convert_ids_to_tokens(token_ids[0])
+tokens = [t.replace("Ġ", "") for t in tokens]
+text_label = "-".join(text.split(" ")[:10])
+expdir = join(savedir, text_label)
+os.makedirs(expdir, exist_ok=True)
+
+outputs, gradvar_stores, act_stores = forward_record_gradhook_vars_all(model, token_ids, )
+max_ids = torch.argmax(outputs.logits[0, -1, :])
+maxlogit = outputs.logits[0, -1, max_ids]
+print(text)
+top_token = tokenizer.convert_ids_to_tokens(max_ids.item()).replace('\u0120','')
+print(f"Top token is '{top_token}', with logit {maxlogit.item():.3f}")
+for sub_module in ["block", "attn", "mlp", "ln_2"]:
+    act_store = [*act_stores[sub_module].values()]
+    grad_maps = torch.autograd.grad(maxlogit,
+                                    [*gradvar_stores[sub_module].values()], retain_graph=True, )  # create_graph=True
+    if grad_maps[0].ndim == 3:
+        grad_map_tsr = torch.cat(grad_maps, dim=0)
+        act_map_tsr = torch.cat(act_store, dim=0)
+    elif grad_maps[0].ndim == 2:
+        grad_map_tsr = torch.stack(grad_maps, dim=0)
+        act_map_tsr = torch.stack(act_store, dim=0)
+    else:
+        raise ValueError
+    grad_map_norm_layers = grad_map_tsr.norm(dim=-1)
+    act_map_norm_layers = act_map_tsr.norm(dim=-1)
+
+    sns.heatmap(grad_map_norm_layers.T)
+    plt.gca().set_yticklabels(tokens, rotation=0)
+    plt.xlabel("Layer")
+    plt.title(f"Gradient L2 norm heat map ({sub_module})")
+    plt.tight_layout()
+    plt.savefig(join(expdir, f'grad_norm_map_layers_{sub_module}.png'))
+    plt.savefig(join(expdir, f'grad_norm_map_layers_{sub_module}.pdf'))
+    plt.show()
+
+    sns.heatmap(act_map_norm_layers.T)
+    plt.gca().set_yticklabels(tokens, rotation=0)
+    plt.xlabel("Layer")
+    plt.title(f"Activation L2 norm heat map ({sub_module})")
+    plt.tight_layout()
+    plt.savefig(join(expdir, f'act_norm_map_layers_{sub_module}.png'))
+    plt.savefig(join(expdir, f'act_norm_map_layers_{sub_module}.pdf'))
+    plt.show()
+
+    sns.heatmap(grad_map_norm_layers.T / act_map_norm_layers.T)
+    plt.gca().set_yticklabels(tokens, rotation=0)
+    plt.xlabel("Layer")
+    plt.title(f"Grad norm / Activation norm raio heat map ({sub_module})")
+    plt.tight_layout()
+    plt.savefig(join(expdir, f'grad_act_ratio_map_layers_{sub_module}.png'))
+    plt.savefig(join(expdir, f'grad_act_ratio_map_layers_{sub_module}.pdf'))
+    plt.show()
+#%%
+savedir = r"F:\insilico_exps\GPT-XL_grad_trace"
+
+text = "The Space Needle is located in downtown"
+text = "On Wednesday, Peter just learned from class that, the Space Needle is located in downtown"
+# text = "Vatican is located in the city of"
+text = "The official residence of the Pope, Vatican is located in the city of"
+# text = "The Apostolic Palace, the official residence of the Pope, is located in the city of"
+# text = "Michael Jackson, one of the most significant cultural figures, was born in"
+# text = "Brian De Palma works in the area of"
+# text = "The headquarter of Zillow is in downtown"
+# text = "Lawrence Taylor professionally plays the sport of"
+# text = "Germaine Greer's domain of work is"
+token_ids = tokenizer.encode(text, return_tensors='pt')
+tokens = tokenizer.convert_ids_to_tokens(token_ids[0])
+tokens = [t.replace("Ġ", "") for t in tokens]
+text_label = "-".join(text.split(" ")[:3])
+expdir = join(savedir, text_label)
+os.makedirs(expdir, exist_ok=True)
+print("Experiment dir: ", expdir)
+for sub_module in ["block", "attn", "mlp", "ln_2"]:
+    outputs, gradvar_store = forward_gradhook_vars(model, token_ids, module_str=sub_module)
+    #% Get the top token and its logit
+    max_ids = torch.argmax(outputs.logits[0, -1, :])
+    maxlogit = outputs.logits[0, -1, max_ids]
+    print(text)
+    top_token = tokenizer.convert_ids_to_tokens(max_ids.item()).replace('\u0120','')
+    print(f"Top token is '{top_token}', with logit {maxlogit.item():.3f}")
+    # torch.save(outputs, join(expdir, "model_outputs_hiddens.pt"))
+    #%% Compute first order gradient
+    grad_maps = torch.autograd.grad(maxlogit,
+                    [*gradvar_store.values()], retain_graph=True,)  #  create_graph=True
+    if grad_maps[0].ndim == 3:
+        grad_map_tsr = torch.cat(grad_maps, dim=0)
+    elif grad_maps[0].ndim == 2:
+        grad_map_tsr = torch.stack(grad_maps, dim=0)
+    else:
+        raise ValueError
+    grad_map_norm_layers = grad_map_tsr.norm(dim=-1)  # torch.cat(grad_map_layers, dim=0)
+    torch.save(grad_map_tsr, join(expdir, f"grad_map_tsr_{sub_module}.pt"))
+    # torch.save(grad_map_norm_layers, join(expdir, f"grad_norm_map_tsr_{sub_module}.pt"))
+    #% Visualize the first order gradient magnitude
+    sns.heatmap(grad_map_norm_layers.T)
+    plt.gca().set_yticklabels(tokens, rotation=0)
+    plt.xlabel("Layer")
+    plt.title(f"Gradient L2 norm heat map ({sub_module})")
+    plt.tight_layout()
+    plt.savefig(join(expdir, f'grad_norm_map_layers_{sub_module}.png'))
+    plt.savefig(join(expdir, f'grad_norm_map_layers_{sub_module}.pdf'))
+    plt.show()
+#%% Compute 1st order gradient with 2nd order graph set up.
+# grad_maps_grad = torch.autograd.grad(maxlogit,
+#                 [*gradvar_store.values()], retain_graph=True, create_graph=True)  #  create_graph=True
+#%%
+import json
+k1000_data = json.load(open("dataset\\known_1000.json", 'r'))
+#%%
+import matplotlib
+matplotlib.use("Agg")
+#%% 990
+for i in range(991, len(k1000_data)):
+    text = k1000_data[i]['prompt']
+    token_ids = tokenizer.encode(text, return_tensors='pt')
+    tokens = tokenizer.convert_ids_to_tokens(token_ids[0])
+    tokens = [t.replace("Ġ", "") for t in tokens]
+    text_label = "-".join(text.split(" ")[:4])
+    expdir = join(savedir, "fact%04d-" % (i) + text_label)
+    os.makedirs(expdir, exist_ok=True)
+
+    outputs, gradvar_stores, act_stores = forward_record_gradhook_vars_all(model, token_ids, )
+    max_ids = torch.argmax(outputs.logits[0, -1, :])
+    maxlogit = outputs.logits[0, -1, max_ids]
+    print(text)
+    top_token = tokenizer.convert_ids_to_tokens(max_ids.item()).replace('\u0120','')
+    print(f"Top token is '{top_token}', with logit {maxlogit.item():.3f}")
+    gradsavedict = {}
+    actsavedict = {}
+    gradmapsavedict = {}
+    actmapsavedict = {}
+    for sub_module in ["block", "attn", "mlp", "ln_2"]:
+        act_store = [*act_stores[sub_module].values()]
+        grad_maps = torch.autograd.grad(maxlogit,
+                                        [*gradvar_stores[sub_module].values()], retain_graph=True, )  # create_graph=True
+        if grad_maps[0].ndim == 3:
+            grad_map_tsr = torch.cat(grad_maps, dim=0)
+            act_map_tsr = torch.cat(act_store, dim=0)
+        elif grad_maps[0].ndim == 2:
+            grad_map_tsr = torch.stack(grad_maps, dim=0)
+            act_map_tsr = torch.stack(act_store, dim=0)
+        else:
+            raise ValueError
+        gradsavedict[sub_module] = grad_map_tsr
+        actsavedict[sub_module] = act_map_tsr
+        grad_map_norm_layers = grad_map_tsr.norm(dim=-1)
+        act_map_norm_layers = act_map_tsr.norm(dim=-1)
+        gradmapsavedict[sub_module] = grad_map_norm_layers
+        actmapsavedict[sub_module]  = act_map_norm_layers
+        plt.figure()
+        sns.heatmap(grad_map_norm_layers.T)
+        plt.gca().set_yticklabels(tokens, rotation=0)
+        plt.xlabel("Layer")
+        plt.title(f"Gradient L2 norm heat map ({sub_module})")
+        plt.tight_layout()
+        plt.savefig(join(expdir, f'grad_norm_map_layers_{sub_module}.png'))
+        plt.savefig(join(expdir, f'grad_norm_map_layers_{sub_module}.pdf'))
+        plt.show()
+
+        plt.figure()
+        sns.heatmap(act_map_norm_layers.T)
+        plt.gca().set_yticklabels(tokens, rotation=0)
+        plt.xlabel("Layer")
+        plt.title(f"Activation L2 norm heat map ({sub_module})")
+        plt.tight_layout()
+        plt.savefig(join(expdir, f'act_norm_map_layers_{sub_module}.png'))
+        plt.savefig(join(expdir, f'act_norm_map_layers_{sub_module}.pdf'))
+        plt.show()
+
+        plt.figure()
+        sns.heatmap(grad_map_norm_layers.T / act_map_norm_layers.T)
+        plt.gca().set_yticklabels(tokens, rotation=0)
+        plt.xlabel("Layer")
+        plt.title(f"Grad norm / Activation norm raio heat map ({sub_module})")
+        plt.tight_layout()
+        plt.savefig(join(expdir, f'grad_act_ratio_map_layers_{sub_module}.png'))
+        plt.savefig(join(expdir, f'grad_act_ratio_map_layers_{sub_module}.pdf'))
+        plt.show()
+
+    # torch.save(savedict, join(expdir, f"grad_act_tsr_save.pt"))
+    # torch.save(mapsavedict, join(expdir, f"grad_act_map_save.pt"))
+    torch.save({"grad" : gradsavedict,
+                "act"  : actsavedict,},
+               join(expdir, f"grad_act_save.pt"))
+    torch.save({"gradmap": gradmapsavedict,
+                "actmap": actmapsavedict},
+               join(expdir, f"grad_act_map_save.pt"))
+    json.dump(k1000_data[i], open(join(expdir,f"knowledge_entry.json"), "w"))
---
+++ core/GPT-XL_record_sparsity.py
@@ -0,0 +1,142 @@
+import pandas as pd
+import torch
+import matplotlib.pyplot as plt
+from transformers import GPT2Model, GPT2Config, GPT2Tokenizer
+from core.layer_hook_utils import featureFetcher_module
+from transformers.models.gpt2.modeling_gpt2 import GPT2Model
+#%%
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2-xl")
+model = GPT2Model.from_pretrained("gpt2-xl")
+# Accessing the model configuration
+configuration = model.config
+model.requires_grad_(False)
+model.eval()
+#%%
+from core.interp_utils import top_tokens_based_on_activation
+from textwrap import wrap
+from colorama import Fore, Back, Style
+def highlight_text_top_words(tokens, activation, tokenizer):
+    """Highlight top words regardless of context. """
+    top_words, bottom_words = top_tokens_based_on_activation(activation, tokens, tokenizer, topk=10, bottomk=10)
+    #%
+    text = tokenizer.convert_ids_to_tokens(tokens, )
+    #%
+    text = [Fore.RED + word + Fore.RESET if word in top_words else word for word in text]
+    text = [Fore.BLUE + word + Fore.RESET if word in bottom_words else word for word in text]
+    text = "".join(text).replace("Ġ", " ") #.replace("Ċ", "\t")
+    # tokenizer.convert_tokens_to_string(text)
+    return text
+
+
+def highlight_text_quantile(tokens, activation, tokenizer, topq=0.75, bottomq=0.25):
+    """Highlight words in context evoking above thresh activity.
+
+    # text_hl_thr = highlight_text_quantile(tok_th[0], fetcher.activations['GPT2_B3_fc'][0, :, 10], tokenizer)
+    # print(text_hl_thr)
+    """
+    top_thr = torch.quantile(activation, topq)
+    bottom_thr = torch.quantile(activation, bottomq)
+    above_top = activation > top_thr
+    below_bottom = activation < bottom_thr
+    text = tokenizer.convert_ids_to_tokens(tokens, )
+    # text = "\n".join(wrap(" ".join(text), 80)).split()
+    text = [Fore.RED + word + Fore.RESET if above_top[i] else word for i, word in enumerate(text)]
+    text = [Fore.BLUE + word + Fore.RESET if below_bottom[i] else word for i, word in enumerate(text)]
+    text = "".join(text).replace("Ġ", " ").replace("Ċ", "\n")
+    return text
+
+
+def model_forward_for_text(text, model, tokenizer, ):
+    tokens = tokenizer.encode(text, return_tensors='pt')
+    with torch.no_grad():
+        outputs = model(tokens)
+
+    return tokens, outputs
+
+
+def record_activations_for_text(text, model, tokenizer, layer_num=11):
+    fetcher = featureFetcher_module()
+    if type(layer_num) not in [tuple, list, range]:
+        layer_num = [layer_num]
+    for layer_i in layer_num:
+        fetcher.record_module(model.h[layer_i].mlp.act, f"GPT2_B{layer_i}_act")
+        fetcher.record_module(model.h[layer_i].mlp.c_fc, f"GPT2_B{layer_i}_fc")
+        fetcher.record_module(model.h[layer_i].mlp.c_proj, f"GPT2_B{layer_i}_proj")
+    tokens = tokenizer.encode(text, return_tensors='pt')
+    with torch.no_grad():
+        outputs = model(tokens)
+    fetcher.cleanup()
+    for k, v in fetcher.activations.items():
+        print(k, v.shape)
+    return tokens, outputs, fetcher
+
+#%%
+
+#%%
+text = """
+Plato was an innovator of the written dialogue and dialectic forms in philosophy. 
+He raised problems for what later became all the major areas of both theoretical philosophy and practical philosophy. His most famous contribution is the theory of Forms known by pure reason, in which Plato presents a solution to the problem of universals, known as Platonism (also ambiguously called either Platonic realism or Platonic idealism).
+He is also the namesake of Platonic love and the Platonic solids.
+"""
+#%%
+text = """
+I went down yesterday to the Piraeus with Glaucon the son of Ariston, that I might offer up my prayers to the goddess; and also because I wanted to see in what manner they would celebrate the festival, which was a new thing. I was delighted with the procession of the inhabitants; but that of the Thracians was equally, if not more, beautiful. When we had finished our prayers and viewed the spectacle, we turned in the direction of the city; and at that instant Polemarchus the son of Cephalus chanced to catch sight of us from a distance as we were starting on our way home, and told his servant to run and bid us wait for him. The servant took hold of me by the cloak behind, and said: Polemarchus desires you to wait.
+I turned round, and asked him where his master was.
+There he is, said the youth, coming after you, if you will only wait.
+Certainly we will, said Glaucon; and in a few minutes Polemarchus appeared, and with him Adeimantus, Glaucon's brother, Niceratus the son of Nicias, and several others who had been at the procession.
+Socrates - POLEMARCHUS - GLAUCON - ADEIMANTUS
+Polemarchus said to me: I perceive, Socrates, that you and our companion are already on your way to the city.
+You are not far wrong, I said.
+But do you see, he rejoined, how many we are?
+Of course.
+And are you stronger than all these? for if not, you will have to remain where you are.
+May there not be the alternative, I said, that we may persuade you to let us go?
+But can you persuade us, if we refuse to listen to you? he said.
+Certainly not, replied Glaucon.
+Then we are not going to listen; of that you may be assured.
+Adeimantus added: Has no one told you of the torch-race on horseback in honour of the goddess which will take place in the evening?
+With horses! I replied: That is a novelty. Will horsemen carry torches and pass them one to another during the race?
+Yes, said Polemarchus, and not only so, but a festival will he celebrated at night, which you certainly ought to see. Let us rise soon after supper and see this festival; there will be a gathering of young men, and we will have a good talk. Stay then, and do not be perverse.
+"""
+
+# layer_num = 0
+tokens, _, fetcher = record_activations_for_text(text, model, tokenizer, layer_num=range(len(model.h)))
+#%%
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+thresh = 1E-2
+#%%
+sparsity_col = []
+for i in range(len(model.h)):
+    acttsr = fetcher[f'GPT2_B{i}_fc']
+    fraction = (acttsr > thresh).sum() / acttsr.numel()
+    sparsity_col.append(fraction)
+
+plt.plot(range(len(model.h)), sparsity_col)
+plt.ylabel("pos activations fraction")
+plt.show()
+#%% Record the fraction of activated hidden units in MLP
+layer_ids = []
+sparsity_col = []
+for layer_i in range(len(model.h)):
+    acttsr = fetcher[f'GPT2_B{layer_i}_fc']
+    fractions = (acttsr > thresh).sum(dim=(0,1)) / acttsr.size(1)
+    sparsity_col.append(fractions)
+    layer_ids.append(layer_i * torch.ones_like(fractions, dtype=torch.int))
+
+layer_ids = torch.cat(layer_ids)
+frac_vec = torch.cat(sparsity_col)
+spars_df = pd.DataFrame({"layer": layer_ids, "fraction":frac_vec})
+#%%
+plt.figure(figsize=[8, 8])
+sns.violinplot(data=spars_df, x="layer", y="fraction", cut=0)
+plt.title("GPT2-XL MLP unit activated fraction")
+plt.ylabel("act > thresh fraction")
+plt.show()
+#%%
+plt.figure(figsize=[6, 4])
+sns.lineplot(data=spars_df, x="layer", y="fraction", )
+plt.title("GPT2-XL MLP unit activated fraction")
+plt.ylabel("act > thresh fraction")
+plt.show()
